796e05de91d224d207ac56696291fc71c4cff665
Márcio Funes <marciofunes@gmail.com>
2023-09-29 18:18:07 -0300

    Create fatec_alunos.sql

---
+++ fatec_alunos.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `fatec_alunos4` (
+  `id` int(11) NOT NULL PRIMARY KEY  AUTO_INCREMENT,
+  `first_name` varchar(250) NOT NULL,
+  `last_name` varchar(250) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;

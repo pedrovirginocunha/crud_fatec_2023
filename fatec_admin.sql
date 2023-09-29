a97acf4a502873d16abd1f2607e70b5c36d6ffd2
Márcio Funes <marciofunes@gmail.com>
2023-09-29 18:16:27 -0300

    Create fatec_admin.sql

---
+++ fatec_admin.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `fatec_admin4` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
